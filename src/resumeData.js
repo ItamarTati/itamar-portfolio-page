461ff300ea1ca3e792bd251ff4dfae3f99278856
Itamar Tati <itamar.tati1994@gmail.com>
2019-02-14 11:41:32 +0000

    Moved folders into new folder

---
+++ src/resumeData.js
@@ -0,0 +1,108 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/ItamarTati",
+    "name": "Itamar Tati",
+    "role": "Frontend Developer",
+    "linkedinId":"b4b2a3102",
+    "discordid": "ET#7565",
+    "roleDescription": "I'm a JavaScript/React Developer looking to learn, gain exprience and meet wonderful people",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/itamar-tati-javascript-developer-react-developer-b4b2a3102/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/ItamarTati",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"https://twitter.com/itamar_rakim",
+          "className":"fa fa-twitter"
+        },
+        {
+          "name":"youtube",
+          "url":"https://www.youtube.com/channel/UCl64Wiuh9WLWQl_a-uaZrlQ/featured",
+          "className":"fa fa-youtube"
+        },
+        {
+          "name":"envelope",
+          "url":"mailto:itamar.tati1994@gmail.com",
+          "className":"fa fa-envelope"
+        }
+      ],
+    "aboutme":"I've been coding as a hobby for more than 2 years creating websites using HTML and CSS, during these years I developed several skills in making websites look beatiful and mordern. More recently I graduted from a JavaScript/React bootcamp where I learned to work with others, make projects using React.js and creating professional projects",
+    "address":"London",
+    "website":"+447568488047",
+    "education":[
+      {
+        "UniversityName":"Middlesex University",
+        "specialization":"Business Management (Innovation Management and Business Start-up)",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2018",
+        "Achievements":"1st Class Degree"
+      },
+      {
+        "UniversityName":"Kodiri Bootcamp",
+        "specialization":"JavaScript & React BootCamp (Google Analytics)",
+        "MonthOfPassing":"Febuary",
+        "YearOfPassing":"2019",
+        "Achievements":"Expert Level (Highest level)"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Help One Child",
+        "specialization":"Charity",
+        "MonthOfLeaving":"December",
+        "YearOfLeaving":"2015",
+        "Achievements":"Assistant Manager"
+      },
+      {
+        "CompanyName":"Eddies",
+        "specialization":"Convience Store",
+        "MonthOfLeaving":"June",
+        "YearOfLeaving":"2013",
+        "Achievements":"Several roles"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Japanese History",
+        "description":"I created a tribute page for one of Japan's most influencial warriors",
+        "imgurl":"images/fanpage copy.png",
+        "url":"https://codepen.io/itamartati/full/oPPKLK"
+      },
+      {
+        "name":"Animeflix",
+        "description":"This is a project I made during my time with Kodiri were I made a Netflix clone based on animes",
+        "imgurl":"images/kodflix copy.png",
+        "url":"https://kodflix-tati.herokuapp.com/"
+      },
+      {
+        "name":"Survey",
+        "description":"This a project I made to gather information about gamers",  
+        "imgurl":"images/survey.png",
+        "url":"https://codepen.io/itamartati/full/QVZOmN"
+      },
+      {
+        "name":"Coding Women",
+        "description":"I created this website as part of a team, our goal was to encourage women to code",
+        "imgurl":"images/coding women.png",
+        "url":"https://codepen.io/itamartati/full/QVZOmN"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
