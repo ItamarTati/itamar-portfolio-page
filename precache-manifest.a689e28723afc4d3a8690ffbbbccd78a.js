7c0cd56ea796904195f5f0ada623ed0117fcffae
Itamar Tati <itamar.tati1994@gmail.com>
2019-02-14 11:45:51 +0000

    Updates

---
+++ precache-manifest.a689e28723afc4d3a8690ffbbbccd78a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6edb06fd2363d7b076ea",
+    "url": "/itamar-portfolio-page/static/js/runtime~main.6edb06fd.js"
+  },
+  {
+    "revision": "e0107b101a4830010c5e",
+    "url": "/itamar-portfolio-page/static/js/main.e0107b10.chunk.js"
+  },
+  {
+    "revision": "6e96b77b8012de2696a9",
+    "url": "/itamar-portfolio-page/static/js/2.6e96b77b.chunk.js"
+  },
+  {
+    "revision": "e0107b101a4830010c5e",
+    "url": "/itamar-portfolio-page/static/css/main.98be0c66.chunk.css"
+  },
+  {
+    "revision": "381cfb14d691879bfec92d8c40040bf3",
+    "url": "/itamar-portfolio-page/index.html"
+  }
+];
