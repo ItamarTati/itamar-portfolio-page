a148acc19ac73c0616eb77894967f54fcf0e0dea
Itamar Tati <itamar.tati1994@gmail.com>
2020-06-29 11:31:51 +0100

    Updates

---
+++ precache-manifest.59b425b8d3629da4ae3addcd301da50e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "76298821eb4e9659cd07",
+    "url": "/itamar-portfolio-pge/static/css/main.4ec78354.chunk.css"
+  },
+  {
+    "revision": "76298821eb4e9659cd07",
+    "url": "/itamar-portfolio-pge/static/js/main.76298821.chunk.js"
+  },
+  {
+    "revision": "cc58b904d5a4c3ed8a15",
+    "url": "/itamar-portfolio-pge/static/js/runtime~main.cc58b904.js"
+  },
+  {
+    "revision": "e6546bb22bd99d1022a4",
+    "url": "/itamar-portfolio-pge/static/js/2.e6546bb2.chunk.js"
+  },
+  {
+    "revision": "a6a5cc41e592274490517f01a6961a22",
+    "url": "/itamar-portfolio-pge/index.html"
+  }
+];
