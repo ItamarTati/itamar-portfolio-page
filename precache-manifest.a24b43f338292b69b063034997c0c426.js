1ed374ebab53071d1644234061f47547a47a5874
Itamar Tati <itamar.tati1994@gmail.com>
2020-06-29 10:26:28 +0100

    Updates

--- precache-manifest.a24b43f338292b69b063034997c0c426.js
+++ precache-manifest.a24b43f338292b69b063034997c0c426.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "2b8d3861abfb7c37cb0d",
+    "revision": "2bf89e1c6a11824f0f43",
     "url": "/itamar-portfolio-page/static/css/main.4ec78354.chunk.css"
   },
   {
-    "revision": "2b8d3861abfb7c37cb0d",
-    "url": "/itamar-portfolio-page/static/js/main.2b8d3861.chunk.js"
+    "revision": "2bf89e1c6a11824f0f43",
+    "url": "/itamar-portfolio-page/static/js/main.2bf89e1c.chunk.js"
   },
   {
     "revision": "6edb06fd2363d7b076ea",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/itamar-portfolio-page/static/js/2.e6546bb2.chunk.js"
   },
   {
-    "revision": "3e300e86d40aa29dae6db7216bf29953",
+    "revision": "ac3e54a01c4f939232ae2ff6d100b254",
     "url": "/itamar-portfolio-page/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/itamar-portfolio-page/precache-manifest.93e3285c6b24c50d80d42a44dcc56e71.js"
+  "/itamar-portfolio-page/precache-manifest.a24b43f338292b69b063034997c0c426.js"
 );
 
 workbox.clientsClaim();
